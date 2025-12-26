b240ae6698bc8a1c892811d3b6cc51d46851609d
shaik <shaik@Azmat>
2025-12-27 03:47:27 +0530

    Look the changes and update

--- tp_30_github/src/test/java/Oraganization_module/dummy_OrgTest.java
+++ tp_30_github/src/test/java/Oraganization_module/dummy_OrgTest.java
@@ -14,6 +14,13 @@ public static void main(String[] args) {
 	System.out.println("----no---");
 	System.out.println("----no---");
 	System.out.println("----no---");
+	System.out.println("----why---");
+	System.out.println("----what---");
+	System.out.println("----how---");
+
+
+
+	
 }
 }
 
