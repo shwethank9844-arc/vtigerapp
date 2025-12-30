2c81bb54dc4e3ad7e636cd9fe70446f7e0c50675
Supriya <Supriya@Santhu>
2025-12-30 17:07:17 +0530

    g

--- tp_30_github/src/test/java/Oraganization_module/dummy_OrgTest.java
+++ tp_30_github/src/test/java/Oraganization_module/dummy_OrgTest.java
@@ -1,8 +1,11 @@
 package Oraganization_module;
 
+import org.testng.annotations.Test;
+
 public class dummy_OrgTest {
 
-public static void main(String[] args) {
+	@Test
+public  void main(String[] args) {
 	System.out.println("----created------");
 	System.out.println("---Yes----");
 	System.out.println("Happy New Year 2026");
