0615b18f7c2ca4159b052b3baf0fcb303c94295c
Supriya <Supriya@Santhu>
2025-12-30 21:17:54 +0530

    jankin

---
+++ tp_30_github/src/test/java/jankinsPratice/pratice1.java
@@ -0,0 +1,15 @@
+package jankinsPratice;
+
+import org.testng.annotations.Test;
+
+public class pratice1 {
+@Test
+public void sample1() {
+	System.out.println("sample 1");
+}
+
+@Test
+public void sample2() {
+	System.out.println("sample2");
+}
+}
