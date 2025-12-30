37ed230caf4215065b3595bc8cfe1fcbd840218c
Supriya <Supriya@Santhu>
2025-12-30 15:49:32 +0530

    code

---
+++ tp_30_github/src/test/java/sample1/demotest.java
@@ -0,0 +1,19 @@
+package sample1;
+
+import org.testng.annotations.Test;
+
+import GEneric_Utils.baseClass;
+
+public class demotest extends baseClass {
+	@Test(groups="regression")
+	public void test() {
+		System.out.println("--demotest1--");
+	}
+	@Test(groups="smoke")
+	public void test2() {
+		System.out.println("--demotest2--");
+	}
+
+
+}
+}
---
+++ tp_30_github/src/test/java/sample1/sampletest.java
@@ -0,0 +1,18 @@
+package sample1;
+
+import org.testng.annotations.Test;
+
+import GEneric_Utils.baseClass;
+
+public class sampletest extends baseClass{
+	 @Test(groups="smoke")
+		public void m1() {
+			System.out.println("--sampletest1--");
+		}
+	@Test
+ public void m2() {
+	 System.out.println("--sampletest2--");
+}
+	
+}
+}
